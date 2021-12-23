ce9899f41248ac70f1f5de300b60ee74a8ab1dea
386083520 <386083520@qq.com>
2021-12-23 20:01:40 +0800

    循环队列其他操作

--- src/queue/LoopQueue.java
+++ src/queue/LoopQueue.java
@@ -32,12 +32,25 @@ public class LoopQueue<E> implements Queue<E>{
 
     @Override
     public E dequeue() {
-        return null;
+        if(isEmpty()) {
+            throw new IllegalArgumentException("队列为空，无法获取");
+        }
+        E e = array[front];
+        array[front] = null;
+        front = (front + 1)% (array.length);
+        size--;
+        if(size == getCapacity()/4) {
+            resize(getCapacity()/2);
+        }
+        return e;
     }
 
     @Override
     public E getFront() {
-        return null;
+        if(isEmpty()) {
+            throw new IllegalArgumentException("队列为空，无法获取");
+        }
+        return array[front];
     }
 
     @Override
@@ -51,11 +64,26 @@ public class LoopQueue<E> implements Queue<E>{
 
     private void resize(int capacity) {
         E[] newArray = (E[])new Object[capacity + 1];
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < size; i++) {
             newArray[i] = array[(i + front) % array.length];
         }
         array = newArray;
         front = 0;
         tail = size;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(String.format("LoopQueue: 容量：%d, 大小：%d\n", getCapacity(), size));
+        stringBuilder.append("front [");
+        for (int i = front; i != tail; i = (i+1)%array.length) {
+            stringBuilder.append(array[i]);
+            if((i%array.length) != (tail - 1)) {
+                stringBuilder.append(",");
+            }
+        }
+        stringBuilder.append("] tail");
+        return stringBuilder.toString();
     }
 }
--- src/queue/Main.java
+++ src/queue/Main.java
@@ -2,10 +2,20 @@ package queue;
 
 public class Main {
     public static void main(String[] args) {
-        Queue<Integer> queue = new ArrayQueue<>();
+        Queue<Integer> queue = new LoopQueue<>();
         for (int i = 0; i < 10; i++) {
             queue.enqueue(i);
         }
+        /*queue.dequeue();
+        queue.dequeue();*/
+        System.out.println(queue);
+        queue.enqueue(11);
+        System.out.println(queue);
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
         queue.dequeue();
         queue.dequeue();
         System.out.println(queue.getFront());
