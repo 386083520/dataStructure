1fad130ac598f6937bfccc939984eb4f2227dd24
386083520 <386083520@qq.com>
2021-12-24 14:46:28 +0800

    链表的改，查等操作

--- src/LinkedList/DummyLinkedList.java
+++ src/LinkedList/DummyLinkedList.java
@@ -54,4 +54,57 @@ public class DummyLinkedList<E> {
         pre.next = node;
         size ++;
     }
+
+    public E get(int index) {
+        if(index < 0 || index >= size) {
+            throw new IllegalArgumentException("获取失败，index不合法");
+        }
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++) { // 1->2->3
+            cur = cur.next;
+        }
+        return cur.e;
+    }
+
+    public E getFirst() {
+        return get(0);
+    }
+
+    public E getLast() {
+        return get(size - 1);
+    }
+
+    public void set(int index, E e) {
+        if(index < 0 || index >= size) {
+            throw new IllegalArgumentException("设置失败，index不合法");
+        }
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        cur.e = e;
+    }
+
+    public boolean contains(E e) {
+        Node cur = dummyHead.next;
+        while (cur != null) {
+            if(cur.e.equals(e)) {
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        Node cur  = dummyHead.next;
+        while (cur != null) {
+            builder.append(cur.e + "->");
+            cur = cur.next;
+        }
+        builder.append("NULL");
+        return builder.toString();
+    }
 }
---
+++ src/LinkedList/Main.java
@@ -0,0 +1,14 @@
+package LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        DummyLinkedList linkedList = new DummyLinkedList();
+        for (int i = 0; i < 10; i++) {
+            linkedList.addFirst(i);
+        }
+        linkedList.add(3, 66);
+        linkedList.set(3, 88);
+        System.out.println(linkedList.contains(66));
+        System.out.println(linkedList);
+    }
+}
