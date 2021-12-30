de41f6c4fc1d3608b06963b29ae84fc9f602e576
386083520 <386083520@qq.com>
2021-12-30 20:43:25 +0800

    二分搜索树添加元素

--- src/BSTree/BSTree.java
+++ src/BSTree/BSTree.java
@@ -5,6 +5,12 @@ public class BSTree<E extends Comparable> {
     private class Node {
         private E e;
         private Node left,right;
+
+        public Node(E e) {
+            this.e = e;
+            this.left = null;
+            this.right = null;
+        }
     }
     private Node root;
     private int size;
@@ -20,5 +26,35 @@ public class BSTree<E extends Comparable> {
 
     public boolean isEmpty() {
         return size == 0;
+    }
+
+    public void add(E e) {
+        if(root == null) {
+            root = new Node(e);
+            size++;
+        }else{
+            add(root, e);
+        }
+    }
+
+    private void add(Node node, E e) {
+        if(e.compareTo(node.e) == 0) {
+            return;
+        }
+        if(e.compareTo(node.e) < 0 && node.left == null) {
+            node.left = new Node(e);
+            size++;
+            return;
+        }
+        if(e.compareTo(node.e) > 0 && node.right == null) {
+            node.right = new Node(e);
+            size++;
+            return;
+        }
+        if(e.compareTo(node.e) < 0) {
+            add(node.left, e);
+        }else {
+            add(node.right, e);
+        }
     }
 }
