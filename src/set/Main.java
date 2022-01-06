811b375d9fe2d78dc3279c9d4bbb7dcc22e41fd4
386083520 <386083520@qq.com>
2022-01-06 19:58:00 +0800

    二分搜索树和链表实现set的时间复杂度对比

--- src/set/Main.java
+++ src/set/Main.java
@@ -2,11 +2,27 @@ package set;
 
 public class Main {
     public static void main(String[] args) {
-        Set<Integer> set = new LinkedListSet<>();
-        int[] arr = {1,2,2,3,3,3,4,4,4,4,5};
+        int[] arr = genArr(100000, 100000);
+        Set<Integer> set1 = new LinkedListSet<>();
+        Set<Integer> set2 = new BSTreeSet<>();
+        test(set1, arr);
+        test(set2, arr);
+    }
+
+    public static void test(Set set, int[] arr) {
+        long startTime = System.nanoTime();
         for (int i = 0; i < arr.length; i++) {
             set.add(arr[i]);
         }
-        System.out.println(set.getSize());
+        long endTime = System.nanoTime();
+        System.out.println((endTime-startTime)/1000000000.0);
+    }
+
+    public static int[] genArr(int len, int max) {
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = (int)(Math.random()*max);
+        }
+        return arr;
     }
 }
