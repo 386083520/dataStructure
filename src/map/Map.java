d8eaf333ad03214d3be6af0a0f58088a0f185b0d
386083520 <386083520@qq.com>
2022-01-07 08:30:44 +0800

    集合基本概念

---
+++ src/map/Map.java
@@ -0,0 +1,11 @@
+package map;
+
+public interface Map<K,V> {
+    void add(K key,V value);
+    V remove(K key);
+    void set(K key,V newValue);
+    boolean contains(K key);
+    V get(K key);
+    boolean isEmpty();
+    int getSize();
+}
